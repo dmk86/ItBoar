4a39839b014f571c68c16afb18d6afa95c9ee9fc
Dominik Szponder <dominik.szponder@pl.abb.com>
2019-03-27 14:47:50 +0100

    Footer modifications

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react'
 import './Footer.css'
+import Hidden from "@material-ui/core/Hidden";
+import Grid from "@material-ui/core/Grid";
 
 class Footer extends Component{
     render() {
         return (
             <div className="footer">
-                <div className="footerContent">
-                    <div className="left"></div>
-                    <div className="center"></div>
-                    <div className="right"></div>
-                </div>
+                <Hidden only={['xs','sm']}>
+                    <div container direction="row" className="footerContent">
+                        <Grid className="left"></Grid>
+                        <Grid className="center"></Grid>
+                        <Grid className="right"></Grid>
+                    </div>
+                </Hidden>
+                <Hidden only={['md', 'lg', 'xl']}>
+                    <div container direction="column" className="footerContent">
+                        <Grid className="top"></Grid>
+                        <Grid className="middle"></Grid>
+                        <Grid className="bottom"></Grid>
+                    </div>
+                </Hidden>
                 <span id="footerSpan">Designed by <a href="https://4coders.pl" target="_blank">4Coders</a></span>
             </div>
         );
