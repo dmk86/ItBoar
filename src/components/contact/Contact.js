38faa2bf8692899f173760ff2bb269e8e2b592d9
Dominik Szponder <dominik.szponder@pl.abb.com>
2019-04-05 14:48:34 +0200

    contact page initialization

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,15 +2,61 @@ import React, {Component} from 'react';
 import './Contact.css'
 import Grid from '@material-ui/core/Grid'
 import Hidden from "@material-ui/core/Hidden/Hidden";
+import TextField from '@material-ui/core/TextField';
 
 class Contact extends Component{
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        text: ''
+    };
+
     render() {
         return (
                 <div className="contact">
                     <Hidden only={['xs','sm']}>
-                        <Grid container direction="row" justify="center" alignItems="center" item xs={6} spacing={32} >
-                            <Grid item xs={4}>
-                                contact
+                        <Grid container direction="row" justify="center" alignItems="flex-start" item xs={12} spacing={32} >
+                            <Grid item xs={32}>
+                                <form noValidate autoComplete="off" justify="center" alignItems="center" >
+                                    <TextField
+                                        id="standard-name"
+                                        label="Name"
+                                        className="textField"
+                                        value=""//{this.state.name}
+                                        // onChange={this.handleChange('name')}
+                                        margin="normal"
+                                        required
+                                    />
+                                    <TextField
+                                        id="standard-name"
+                                        label="Phone"
+                                        className="textField"
+                                        value=""//{this.state.name}
+                                        // onChange={this.handleChange('name')}
+                                        margin="normal"
+                                    />
+                                    <TextField
+                                        id="standard-name"
+                                        label="Email"
+                                        className="textField"
+                                        value=""//{this.state.name}
+                                        // onChange={this.handleChange('name')}
+                                        margin="normal"
+                                        required
+                                    />
+                                    <TextField
+                                    id="standard-textarea"
+                                    label="Text"
+                                    required
+                                    placeholder="Placeholder"
+                                    multiline
+                                    className="textField"
+                                    margin="normal"
+                                    rows={10}
+                                    fullWidth={ true}
+                                    />
+                                </form>
                             </Grid>
                         </Grid>
                     </Hidden >
